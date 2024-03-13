d02bea6df716f42cdee8da7c230e6b0b1a929d85
juanesgv <juanesgarviedma23@gmail.com>
2024-03-12 21:21:12 -0500

    Control props alternativo

--- src/02-component.pattern/hooks/useShoppingCard.ts
+++ src/02-component.pattern/hooks/useShoppingCard.ts
@@ -9,33 +9,18 @@ export  const useShoppingCart = () => {
 
         setShoppingCart(prevShoppingCart => {
 
-            const productInCart: ProductInCart = prevShoppingCart[product.id] || { ...product, count: 0 }
+            if(count === 0){
 
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...prevShoppingCart,
-                    [product.id]: productInCart
-                }
+                const { [product.id]: toDelete, ...rest} = prevShoppingCart
+                console.log("el que voy a eliminar", toDelete)
+
+                return rest
             }
 
-            const { [product.id]: toDelete, ...rest } = prevShoppingCart
-            console.log("el que voy a eliminar", toDelete)
-            return rest
-
-
-            // if(count === 0){
-
-            //     const { [product.id]: toDelete, ...rest} = prevShoppingCart
-            //     console.log("el que voy a eliminar", toDelete)
-
-            //     return rest
-            // }
-
-            // return {
-            //     ...prevShoppingCart,
-            //     [product.id]: {...product, count}
-            // }
+            return {
+                ...prevShoppingCart,
+                [product.id]: {...product, count}
+            }
 
         })
 
